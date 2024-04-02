ff8c61c152ea68e2176b3e25137066f7e14b5d1f
amit1698 <shete4@gmail.com>
2024-04-02 12:02:03 +0530

    java file added

---
+++ Anagram.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Anagram
+
+{
+public static void main(String args[])
+
+{
+String s = args[0];
+String s1 = args[1];
+
+if(s.length()!=s1.length())
+{
+	System.out.println("given string are not  anagram");
+
+}
+
+char [] a = s.toCharArray();
+char [] b = s1.toCharArray();
+
+Arrays.sort(a);
+Arrays.sort(b);
+
+boolean flag = Arrays.equals(a,b);
+if(flag)
+{
+ System.out.println("anagrams");
+}
+else
+{
+ System.out.println("not anagrams");
+}
+
+}
+}
